eb02f0c5b14aef832b7a88d877732d1d55a851f9
Adriano Ivan de Paula Campos <adrianoivandepaulacampos@gmail.com>
2024-07-09 22:11:23 -0300

    initial commit

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel()
+            {
+                Name = "Developer",
+                DateOfBirth = new DateTime(2002, 06, 17)
+            };
+
+            return View(data);
+        }
+    }
+}
